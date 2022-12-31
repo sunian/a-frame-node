16d33c4d422b05956bd7806da778228451c82b53
James Sun <jamessun92@gmail.com>
2022-12-30 21:21:31 -0500

    added sample IK chain

--- src/components/move-arm.js
+++ src/components/move-arm.js
@@ -26,36 +26,63 @@ AFRAME.registerComponent("move-arm", {
       const findBone = function (name) {
         return bones.find((b) => b.name.includes(name));
       };
-      this.armBone = findBone(this.data.arm + "ForeArm");
-      this.ikBones = [];
+      this.armBone = findBone(this.data.arm + "Shoulder");
+      console.log(this.armBone);
       const ik = new THREE.IK();
+
       const chain = new THREE.IKChain();
-      const constraints = [new THREE.IKBallConstraint()];
+      const flexConstraint = [new THREE.IKBallConstraint(120)];
+      const fixedConstraint = [new THREE.IKBallConstraint(0)];
+      this.tempBones = [];
 
-      // first
-      chain.add(
-        new THREE.IKJoint(findBone(this.data.arm + "Shoulder"), { constraints })
+      // Create a target that the IK's effector will reach
+      // for.
+      const movingTarget = new THREE.Mesh(
+        new THREE.SphereGeometry(0.1),
+        new THREE.MeshBasicMaterial({ color: 0xff0000 })
       );
-      chain.add(
-        new THREE.IKJoint(findBone(this.data.arm + "Arm"), { constraints })
-      );
-      chain.add(
-        new THREE.IKJoint(findBone(this.data.arm + "ForeArm"), { constraints })
-      );
+      this.pivot = new THREE.Object3D();
+      this.pivot.add(movingTarget);
+      scene.add(this.pivot);
 
-      // last
-      chain.add(
-        new THREE.IKJoint(findBone(this.data.arm + "Hand"), { constraints }),
-        { target: this.target }
-      );
+      // Create a chain of THREE.Bone's, each wrapped as an IKJoint
+      // and added to the IKChain
+      for (let i = 0; i <= 3; i++) {
+        const bone = new THREE.Bone();
+        if (i === 0) {
+          // bone.position.copy(this.armBone.position);
+          bone.position.y = 0;
+        } else {
+          bone.position.y = 0.5;
+        }
+
+        if (this.tempBones[i - 1]) {
+          this.tempBones[i - 1].add(bone);
+        }
+        this.tempBones.push(bone);
+
+        // The last IKJoint must be added with a `target` as an end effector.
+        const target = i === 3 ? movingTarget : null;
+        const constraints = i === 0 ? fixedConstraint : flexConstraint;
+        chain.add(new THREE.IKJoint(bone, { constraints }), { target });
+      }
+
+      // Add the chain to the IK system
       ik.add(chain);
-      // scene.add(ik.getRootBone());
+
+      // Ensure the root bone is added somewhere in the scene
+      scene.add(ik.getRootBone());
+
+      // Create a helper and add to the scene so we can visualize
+      // the bones
+      const helper = new IKHelper(ik);
+      scene.add(helper);
       this.iks.push(ik);
     });
   },
 
   tick: function (time, timeDelta) {
-    this.target.position.copy(this.leftController.position);
+    this.pivot.position.copy(this.leftController.position);
     for (let ik of this.iks) {
       ik.solve();
     }
